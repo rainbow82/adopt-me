9b457eb434f226c7f122ee850454c92d843a5239
Shannon Beach <shannonbeach1982@gmail.com>
2020-09-08 08:39:49 -0400

    project setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, { name: 'Luna', animal: 'dog', breed: 'Yorkie' }),
+    React.createElement(Pet, { name: 'Diego', animal: 'dog', breed: 'Yorkie' }),
+    React.createElement(Pet, { name: 'Bruno', animal: 'dog', breed: 'Yorkie' })
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
