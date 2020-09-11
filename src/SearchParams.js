945deb907a1ed297780c7b9f84842cc49dd455d5
Shannon Beach <shannonbeach1982@gmail.com>
2020-09-11 19:45:31 -0400

    added breeds search box

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -4,6 +4,8 @@ import { ANIMALS } from '@frontendmasters/pet';
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
   const [animal, setAnimal] = useState('dog');
+  const [breed, setBreed] = useState('');
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -27,7 +29,22 @@ const SearchParams = () => {
           >
             <option>All</option>
             {ANIMALS.map(
-              animal => <option value={animal}>{animal}</option>
+              animal => <option key={animal} value={animal}>{animal}</option>
+            )}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          breed
+          <select
+            id='breed'
+            value={breed}
+            onChange={e => setBreed(e.target.breed)}
+            onBlur={(e) => setAnimal(e.target.value)}
+            disabled={!breeds.length}
+          >
+            <option>All</option>
+            {breeds.map(
+              breed => <option key={breed} value={breed}>{breed}</option>
             )}
           </select>
         </label>
