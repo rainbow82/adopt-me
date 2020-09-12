36cc92ade3ae2dd4e0f9bd480884c28056318f3a
Shannon Beach <shannonbeach1982@gmail.com>
2020-09-12 13:53:14 -0400

    added petfinder api call

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropDown';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
   const [breeds, setBreeds] = useState([]);
-  const [animal, AnimalDropdown] = useDropdown('Animal', "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
+  const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
 
+  useEffect(() => {
+    setBreeds([]);
+    setBreed('');
 
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error.error);
+  }, [animal, setBreeds, setBreed]);
 
   return (
     <div className="search-params">
@@ -22,8 +30,8 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
-        <AnimalDropdown/>
-        <BreedDropdown/>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
